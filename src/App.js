7007dd0d98887fc898682505c206584de6afb2ec
Vasile Luca <vasile.luca@tickbird.com>
2023-09-28 12:00:27 +0300

    og added

--- src/App.js
+++ src/App.js
@@ -22,21 +22,8 @@ function App() {
   return (
     <div className='App' style={{ backgroundColor: '#dff5ce7a' }}>
       <AuthContextProvider>
-        <Nav />
-
         <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
+          <Nav />
         </header>
 
         <Routes>
