90bc1ac273dd7871d25373341a921a4b3c0d47f3
Vasile Luca <vasile.luca@tickbird.com>
2023-09-28 11:26:12 +0300

    header fix

--- src/App.js
+++ src/App.js
@@ -23,21 +23,20 @@ function App() {
     <div className='App' style={{ backgroundColor: '#dff5ce7a' }}>
       <AuthContextProvider>
         <Nav />
+
         <header className='App-header'>
-          <header className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className='App-link'
-              href='https://reactjs.org'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Learn React
-            </a>
-          </header>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
         </header>
 
         <Routes>
