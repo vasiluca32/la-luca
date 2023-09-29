b9de8f8c608e5f9afc78d54ec222b7ceb1c09e98
Vasile Luca <vasile.luca@tickbird.com>
2023-09-29 17:16:55 +0300

    meta+storage ok

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Blog = () => {
   return (
-    <div style={{ marginTop: '60px' }}>
-      <h1>Blog page</h1>
-    </div>
+    <>
+      <Helmet>
+        <title>Blog - Gradina La Luca</title>
+        <meta property='og:title' content='Blog - Gradina La Luca' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://la-luca.web.app/blog' />
+        <meta
+          property='og:image'
+          content='https://firebasestorage.googleapis.com/v0/b/la-luca.appspot.com/o/appAssets%2Fheading-image.jpg?alt=media&token=56d8a9bb-98c6-484c-81bb-3c270c835fa6'
+        />
+      </Helmet>
+      <div style={{ marginTop: '60px' }}>
+        <h1>Blog page</h1>
+      </div>
+    </>
   );
 };
 
