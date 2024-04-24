9c57dc2bd2261e775a6e4a5c7640bed573d77f6c
Vasile Luca <vasile.luca@tickbird.com>
2024-04-24 17:36:17 +0300

    refresh fixed

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,7 +6,7 @@ import { getStorage } from 'firebase/storage';
 import { getFunctions } from 'firebase/functions';
 // import { connectFunctionsEmulator } from 'firebase/functions';
 // import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-// import { connectDatabaseEmulator } from 'firebase/database';
+import { connectDatabaseEmulator } from 'firebase/database';
 import { getDatabase, ref } from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -28,7 +28,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase();
+
 export const firestoreDb = getFirestore();
+
+const location = window.location;
+
+// if (location.hostname === 'localhost') {
+//   // Point to the RTDB emulator running on localhost.
+//   connectDatabaseEmulator(db, '127.0.0.1', 9000);
+// }
 
 export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
